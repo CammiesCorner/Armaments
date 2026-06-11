b761b06ca1f9e12b463d7d7ff805ebee5e9a53bb
CammiePone <inazuma261@gmail.com>
2026-06-11 09:45:44 -0700

    fix one

--- src/main/java/dev/cammiescorner/armaments/common/items/SeaCrownItem.java
+++ src/main/java/dev/cammiescorner/armaments/common/items/SeaCrownItem.java
@@ -1,23 +1,29 @@
 package dev.cammiescorner.armaments.common.items;
 
+import dev.cammiescorner.armaments.Armaments;
 import dev.cammiescorner.armaments.ArmamentsConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.Level;
 
 import java.util.List;
 
 public class SeaCrownItem extends ArmorItem implements Equipable {
 	public SeaCrownItem(Properties settings) {
-		super(ArmorMaterials.TURTLE, Type.HELMET, settings);
+		super(ArmorMaterials.TURTLE, Type.HELMET, settings.attributes(createAttributes()));
 	}
 
 	@Override
@@ -55,16 +61,14 @@ public class SeaCrownItem extends ArmorItem implements Equipable {
 		}
 	}
 
-	// TODO fix
-//	@Override
-//	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot slot) {
-//		ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-//
-//		if(ArmamentsConfig.SeaCrown.healthModifier != 0)
-//			builder.put(Attributes.MAX_HEALTH, new AttributeModifier(Armaments.id("crown_max_health"), ArmamentsConfig.SeaCrown.healthModifier, AttributeModifier.Operation.ADD_VALUE));
-//		if(ArmamentsConfig.SeaCrown.armorPoints != 0)
-//			builder.put(Attributes.ARMOR, new AttributeModifier(ArmorItem.ARMOR_MODIFIER_UUID_PER_TYPE.get(getType()), "Crown modifier", ArmamentsConfig.SeaCrown.armorPoints, AttributeModifier.Operation.ADD_VALUE));
-//
-//		return slot == EquipmentSlot.HEAD ? builder.build() : super.getDefaultAttributeModifiers(slot);
-//	}
+	public static ItemAttributeModifiers createAttributes() {
+		var builder = ItemAttributeModifiers.builder();
+
+		if(ArmamentsConfig.SeaCrown.healthModifier != 0)
+			builder.add(Attributes.MAX_HEALTH, new AttributeModifier(Armaments.id("crown_health_cost"), ArmamentsConfig.SeaCrown.healthModifier, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD);
+		if(ArmamentsConfig.SeaCrown.armorPoints != 0)
+			builder.add(Attributes.ARMOR, new AttributeModifier(ResourceLocation.withDefaultNamespace("armor." + Type.HELMET.getName()), ArmamentsConfig.SeaCrown.armorPoints, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD);
+
+		return builder.build();
+	}
 }
