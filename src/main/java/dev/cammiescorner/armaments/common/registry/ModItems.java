d8887da4c4ed37bfedb97e64665805b7971e9d4f
CammiePone <inazuma261@gmail.com>
2026-06-13 07:57:57 -0700

    sea crown stacks to 1, riptide cool down implemented

--- src/main/java/dev/cammiescorner/armaments/common/registry/ModItems.java
+++ src/main/java/dev/cammiescorner/armaments/common/registry/ModItems.java
@@ -19,7 +19,7 @@ import java.util.function.Supplier;
 public class ModItems {
 	public static final RegistryHandler<Item> ITEMS = RegistryHandler.create(Registries.ITEM, Armaments.MOD_ID);
 
-	public static final RegistrySupplier<Item> SEA_CROWN = ITEMS.register("sea_crown", () -> new SeaCrownItem(new Item.Properties()));
+	public static final RegistrySupplier<Item> SEA_CROWN = ITEMS.register("sea_crown", () -> new SeaCrownItem(new Item.Properties().stacksTo(1)));
 	public static final RegistrySupplier<Item> CRYSTAL_SPEAR = ITEMS.register("crystal_spear", () -> new CrystalSpearItem(ToolMaterials.AMETHYST, new Item.Properties()));
 //	public static final RegistrySupplier<Item> BLUNDERBUSS = ITEMS.register("blunderbuss", () -> new BlunderbussItem(new FabricItemSettings().maxCount(1)));
 	public static final RegistrySupplier<Item> ECHO_DAGGER = ITEMS.register("echo_dagger", () -> new EchoDaggerItem(new Item.Properties().stacksTo(1)));
--- src/main/java/dev/cammiescorner/armaments/mixin/TridentItemMixin.java
+++ src/main/java/dev/cammiescorner/armaments/mixin/TridentItemMixin.java
@@ -11,6 +11,8 @@ import net.minecraft.world.item.TridentItem;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TridentItem.class)
 public class TridentItemMixin {
@@ -22,5 +24,11 @@ public class TridentItemMixin {
 	@ModifyExpressionValue(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isInWaterOrRain()Z"))
 	private boolean stopUsingRiptide(boolean original, ItemStack stack, Level world, LivingEntity user) {
 		return original || (ArmamentsConfig.SeaCrown.enablesRiptide && user.getItemBySlot(EquipmentSlot.HEAD).is(ModItems.SEA_CROWN.get()));
+	}
+
+	@Inject(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;startAutoSpinAttack(IFLnet/minecraft/world/item/ItemStack;)V"))
+	private void riptideCoolDown(ItemStack itemStack, Level level, LivingEntity livingEntity, int i, CallbackInfo ci) {
+		if(ArmamentsConfig.SeaCrown.enablesRiptide && livingEntity instanceof Player player && player.getItemBySlot(EquipmentSlot.HEAD).is(ModItems.SEA_CROWN.get()))
+			player.getCooldowns().addCooldown(itemStack.getItem(), ArmamentsConfig.SeaCrown.riptideCoolDown);
 	}
 }
